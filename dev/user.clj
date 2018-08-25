94eb846af5d914492c8ddf810ba405c996383200
#movingtogitlab <francois.deserres@gmail.com>
2018-08-25 20:51:09 +0800

    CHANGE: fetcher also returns history

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,6 @@
 (ns user
   (:require
-   [figwheel-sidecar.repl-api :as f]
+   ; [figwheel-sidecar.repl-api :as f]
    [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
 
 ;; user is a namespace that the Clojure runtime looks for and
@@ -20,24 +20,24 @@
 ;; and Component https://github.com/stuartsierra/component
 
 
-(defn fig-start
-  "This starts the figwheel server and watch based auto-compiler."
-  []
-  ;; this call will only work are long as your :cljsbuild and
-  ;; :figwheel configurations are at the top level of your project.clj
-  ;; and are not spread across different lein profiles
-
-  ;; otherwise you can pass a configuration into start-figwheel! manually
-  (f/start-figwheel!))
-
-(defn fig-stop
-  "Stop the figwheel server and watch based auto-compiler."
-  []
-  (f/stop-figwheel!))
-
-;; if you are in an nREPL environment you will need to make sure you
-;; have setup piggieback for this to work
-(defn cljs-repl
-  "Launch a ClojureScript REPL that is connected to your build and host environment."
-  []
-  (f/cljs-repl))
+; (defn fig-start
+;   "This starts the figwheel server and watch based auto-compiler."
+;   []
+;   ;; this call will only work are long as your :cljsbuild and
+;   ;; :figwheel configurations are at the top level of your project.clj
+;   ;; and are not spread across different lein profiles
+;
+;   ;; otherwise you can pass a configuration into start-figwheel! manually
+;   (f/start-figwheel!))
+;
+; (defn fig-stop
+;   "Stop the figwheel server and watch based auto-compiler."
+;   []
+;   (f/stop-figwheel!))
+;
+; ;; if you are in an nREPL environment you will need to make sure you
+; ;; have setup piggieback for this to work
+; (defn cljs-repl
+;   "Launch a ClojureScript REPL that is connected to your build and host environment."
+;   []
+;   (f/cljs-repl))
--- src/cpp/fetcher/core.clj
+++ src/cpp/fetcher/core.clj
@@ -2,7 +2,9 @@
   (:require
    [clojure.set :as set]
    [clojure.pprint :refer [pprint]]
-   [cheshire.core :as json])
+   [cheshire.core :as json]
+   [clj-time.core :as t]
+   [clj-time.coerce :as tc])
   (:import
    [java.net URLEncoder]))
 
@@ -31,16 +33,47 @@
                         "Algorithm" :algorithm
                         "ProofType" :proof-type})))
 
-(defn get-top-coins
+(defn coin-history
+  "Fetch daily historical data for a coin.
+   sym: coin symbol.
+   return: a seq of maps with keys :date :open :high :low :close
+   :volume-from :volume-to."
+  [sym]
+  (let [uri (str api-host
+                (format "/data/histoday?fsym=%s&tsym=USD&limit=5000" sym)
+                extra-params)
+        raw-data (get (json/parse-string (slurp uri)) "Data")
+        extract (fn [x] (-> x
+                            (set/rename-keys {"time" :time
+                                              "open" :open
+                                              "high" :high
+                                              "low" :low
+                                              "close" :close
+                                              "volumefrom" :volume-from
+                                              "volumeto" :volume-to})
+                            (update :time #(java.util.Date. (* 1000 (long %))))))]
+    (map extract raw-data)))
+
+(defn top-coins
   "Fetch the current top coins (total volume across all markets).
-   num: number (eg. 50 for top 50). If less than 10, top 10 is assumed.
+   num: number (eg. 50 for top 50). If less than 10, top 10 is assumed by the API.
    return: a vector of maps (see fn `coin-info`)."
   [num]
   (let [uri (str api-host
                  (format "/data/top/totalvol?limit=%s&tsym=USD" num)
                  extra-params)
-        raw-coins (get (json/parse-string (slurp uri)) "Data")]
-    (map coin-info raw-coins)))
+        raw-data (get (json/parse-string (slurp uri)) "Data")
+        extract  (fn [x] (-> x
+                             (get "CoinInfo")
+                             (select-keys ["Name" "FullName" "ImageUrl" "Algorithm" "ProofType"])
+                             (set/rename-keys {"Name" :symbol
+                                               "FullName" :full-name
+                                               "ImageUrl" :image-url
+                                               "Algorithm" :algorithm
+                                               "ProofType" :proof-type})))]
+    (->> raw-data
+         (map extract)
+         (map #(assoc % :history (coin-history (:symbol %)))))))
 
 (defn coins->files!
   [coins]
@@ -64,7 +97,7 @@
 
 (defn -main [& args]
   (.mkdir (java.io.File. "resources/public/data"))
-  (coins->files! (get-top-coins 10))
+  (coins->files! (top-coins 10))
 
   ; (get-coin-price "BTC")
   ; (println (slurp (str "resources/public/data/" "BTC" "_price.edn")))
