3d8256a4817d506755536df3f0e26a3450177485
François De Serres <fdserr@users.noreply.github.com>
2018-11-16 12:12:33 +0800

    ch02

--- chapter02/web-coins/src/repl.clj
+++ chapter02/web-coins/src/repl.clj
@@ -1,90 +1,156 @@
 (ns repl)
 
-(comment
+(require '[org.httpkit.server :as server])
+(defn webapp
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>My Server</h1>"})
+(def stop (server/run-server #'webapp {:port 8080}))
 
- (require '[org.httpkit.server :as server])
- (defn webapp
-   [req]
-   {:status 200
-    :headers {"Content-Type" "text/html"}
-    :body "<h1>My Server</h1>"})
- (def stop (server/run-server #'webapp {:port 8080}))
+;;;redef webapp, check browser
+(defn webapp
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "<h1>My Hot Server</h1>"})
 
- ;;;redef webapp, check browser
- (defn webapp
-   [req]
-   {:status 200
-    :headers {"Content-Type" "text/html"}
-    :body "<h1>My Hot Server</h1>"})
+;;;add routing
+(require '[compojure.core :refer [defroutes GET]])
+(defroutes webapp
+  (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>")))
 
- ;;;add routing
- (require '[compojure.core :refer [defroutes GET]])
- (defroutes webapp
-   (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>")))
+(defroutes webapp
+  (GET "/" req "<h1>Home</h1>")
+  (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>")))
 
- (defroutes webapp
-   (GET "/" req "<h1>Home</h1>")
-   (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>")))
+;;;add html
+(require '[hiccup.core :refer [html]])
+(defn html-nav
+  [active-uri]
+  (html
+   [:nav
+    (if (#{"" "/"} active-uri)
+      [:span "Home"]
+      [:a {:href "/"} "Home"])
+    [:span " | "]
+    (if (= "/req" active-uri)
+      [:span "Request"]
+      [:a {:href "/req"} "Request"])]))
+(html-nav "/req")
+(defn html-home
+  []
+  (html
+   [:body
+    (html-nav)
+    [:main
+     [:h1 "My Hiccup Home"]
+     [:p "This is my homepage, made with Hiccup!"]]]))
+(defn html-req
+  [req]
+  (html
+   [:body
+    (html-nav)
+    [:main
+     [:h1 "Request map:"]
+     [:pre (with-out-str (pprint req))]]]))
+(defn handle-home
+  [_]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (html-home)})
+(defn handle-req
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (html-req req)})
+(defroutes webapp
+  (GET "/" req handle-home)
+  (GET "/req" req handle-req))
 
- ;;;add html
- (require '[hiccup.core :refer [html]])
- (defn page-home
-   []
-   (html [:h1 "My Hiccup Home"]))
- (defn handle-home
-   [_]
-   {:status 200
-    :headers {"Content-Type" "text/html"}
-    :body (page-home)})
- (defroutes webapp
-   (GET "/" req handle-home)
-   (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>")))
+(defn page-home
+  []
+  (html [:h1 "My New Hiccup Home"]))
 
- (defn page-home
-   []
-   (html [:h1 "My New Hiccup Home"]))
+;;;use markup files
+(require '[web-coins.ui :as ui :reload-all true])
+(defn handle-home
+  [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (ui/page-home req)})
 
- ;;;use markup files
- (require '[web-coins.ui :as ui :reload-all true])
- (defn handle-home
-   [req]
-   {:status 200
-    :headers {"Content-Type" "text/html"}
-    :body (ui/page-home req)})
+;;;wrapping
 
- ;;;reload
- (require '[ring.middleware.reload :refer [wrap-reload]])
- (stop)
- (def stop (server/run-server (wrap-reload webapp) {:port 8080}))
+(defn say [s]
+  (println s))
+(defn wrap-hello [f]
+  (fn [s]
+    (apply f [(str "hello " s)])))
+(def say-hello (wrap-hello say))
+(say-hello "world")
 
- ;;;ws
- (require '[org.httpkit.timer :as timer])
- (defn handle-ticker
-   [req]
-   (server/with-channel req channel
-     (server/on-close channel (fn [status] (println "channel closed, " status)))
-     (loop [id 0]
-       (when (< id 1000)
-         (timer/schedule-task
-          (* id 200) ;; send a message every 200ms
-          (server/send! channel (str "Tick from server #" id) false)) ; false => don't close after send
-         (recur (inc id))))
-     (timer/schedule-task 100000 (server/close channel)))) ;; close in 100s.
+(defn shout [s]
+  (println (.toUpperCase s)))
+(def shout-hello (wrap-hello shout))
+(shout-hello "world")
 
- (stop)
+(defn wrap-bang [f]
+  (fn [s]
+    (apply f [(str s "!!!")])))
+(def say-hello-bang
+  (-> say
+      (wrap-hello)
+      (wrap-bang)))
+(say-hello-bang "world")
+(def shout-hello-bang
+  (-> shout
+      (wrap-hello)
+      (wrap-bang)))
+(shout-hello-bang "world")
 
- (require '[compojure.route :refer [resources not-found]])
- (defroutes webapp
-   (GET "/" req handle-home)
-   (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>"))
-   (GET "/ticker" req handle-ticker)
-   (resources "/")
-   (not-found "NOT FOUND"))
+(def shout-bang-hello
+  (-> shout
+      (wrap-bang)
+      (wrap-hello)))
+(shout-bang-hello "world")
 
- (def stop (server/run-server (wrap-reload webapp) {:port 8080}))
+(defn divide [a b] ())
 
 
- nil)
+
+;;;reload
+(require '[ring.middleware.reload :refer [wrap-reload]])
+(stop)
+(def stop (server/run-server (wrap-reload webapp) {:port 8080}))
+
+;;;ws
+(require '[org.httpkit.timer :as timer])
+(defn handle-ticker
+  [req]
+  (server/with-channel req channel
+    (server/on-close channel (fn [status] (println "channel closed, " status)))
+    (loop [id 0]
+      (when (< id 1000)
+        (timer/schedule-task
+         (* id 200) ;; send a message every 200ms
+         (server/send! channel (str "Tick from server #" id) false)) ; false => don't close after send
+        (recur (inc id))))
+    (timer/schedule-task 100000 (server/close channel)))) ;; close in 100s.
+
+(stop)
+
+(require '[compojure.route :refer [resources not-found]])
+(defroutes webapp
+  (GET "/" req handle-home)
+  (GET "/req" req (str "<pre>" (with-out-str (pprint req)) "</pre>"))
+  (GET "/ticker" req handle-ticker)
+  (resources "/")
+  (not-found "NOT FOUND"))
+
+(def stop (server/run-server (wrap-reload webapp) {:port 8080}))
+
+
 
 
 (comment
